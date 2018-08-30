d54c0d2e1549dab2ff77f2a5adc837d5d22bcc40
Stefan Rosu <rosu@outlook.com>
2018-08-30 18:20:17 +0300

    ObjectOriented initial

---
+++ DesignPatterns/OO/After/Account.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace OO.After
+{
+    public class Account
+    {
+        private readonly Action _onUnfreeze;
+        private bool _isverified;
+        private bool _isClosed;
+        private bool _isFrozen;
+
+
+        public Account(Action onUnfreeze)
+        {
+            _onUnfreeze = onUnfreeze;
+            this.ManageUnfreezing = StayUnfrozen;
+        }
+
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal amount)
+        {
+            if (_isClosed)
+            {
+                return;
+            }
+
+            ManageUnfreezing();
+
+            Balance += amount;
+        }
+
+        private Action ManageUnfreezing { get; set; }
+
+        private void StayUnfrozen()
+        {
+        }
+
+        private void Unfreeze()
+        {
+            _isFrozen = false;
+            _onUnfreeze();
+            ManageUnfreezing = StayUnfrozen;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (!_isverified)
+            {
+                return;
+            }
+
+            if (_isClosed)
+            {
+                return;
+            }
+
+            ManageUnfreezing();
+
+            Balance -= amount;
+        }
+
+        public void Verify()
+        {
+            _isverified = true;
+        }
+
+        public void Close()
+        {
+            _isClosed = true;
+        }
+
+        public void Freeze()
+        {
+            if (!_isverified)
+            {
+                return;
+            }
+
+            if (_isClosed)
+            {
+                return;
+            }
+
+            _isFrozen = true;
+            this.ManageUnfreezing = Unfreeze;
+        }
+    }
+}
---
+++ DesignPatterns/OO/Initial/Account.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace OO.Initial
+{
+    public class Account
+    {
+        private readonly Action _onUnfreeze;
+        private bool _isverified;
+        private bool _isClosed;
+        private bool _isFrozen;
+
+
+        public Account(Action onUnfreeze)
+        {
+            _onUnfreeze = onUnfreeze;
+        }
+
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal amount)
+        {
+            if (_isClosed)
+            {
+                return;
+            }
+
+            if (_isFrozen)
+            {
+                _isFrozen = false;
+                _onUnfreeze();
+            }
+
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (!_isverified)
+            {
+                return;
+            }
+
+            if (_isClosed)
+            {
+                return;
+            }
+
+            if (_isFrozen)
+            {
+                _isFrozen = false;
+                _onUnfreeze();
+            }
+
+            Balance -= amount;
+        }
+
+        public void Verify()
+        {
+            _isverified = true;
+        }
+
+        public void Close()
+        {
+            _isClosed = true;
+        }
+
+        public void Freeze()
+        {
+            if (!_isverified)
+            {
+                return;
+            }
+
+            if (_isClosed)
+            {
+                return;
+            }
+
+            _isFrozen = true;
+        }
+    }
+}
