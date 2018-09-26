4a9b0c9b2853e3b247a60640d376873e57ed853d
Stefan Rosu <rosu@outlook.com>
2018-09-26 18:38:58 +0300

    OO - Immutability

---
+++ DesignPatterns/OO-Imutable/MoneyAmount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OO_Imutable
+{
+    public sealed class MoneyAmount : IEquatable<MoneyAmount>
+    {
+        public decimal Amount { get; }
+        public string Currency { get; }
+
+        public MoneyAmount(decimal amount, string currency)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Amount negative");
+            }
+
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public MoneyAmount Scale(decimal factor) => new MoneyAmount(Amount * factor, Currency);
+
+        public static MoneyAmount operator *(MoneyAmount amount, decimal factor) => amount.Scale(factor);
+
+        public override string ToString() => $"{Amount} {Currency}";
+
+        public override bool Equals(object obj) =>
+            Equals(obj as MoneyAmount);
+
+        public override int GetHashCode()
+        {
+            // return Amount.GetHashCode() ^ Currency.GetHashCode();
+            return new {Amount, Currency}.GetHashCode();
+        }
+
+        public static bool operator ==(MoneyAmount a, MoneyAmount b) => 
+            (object.ReferenceEquals(a, null) && object.ReferenceEquals(b, null)) || 
+            (!object.ReferenceEquals(a, null) && a.Equals(b));
+
+        public static bool operator !=(MoneyAmount a, MoneyAmount b) => !(a == b);
+
+        public bool Equals(MoneyAmount other) =>
+            other != null &&
+            Amount == other.Amount &&
+            Currency == other.Currency;
+    }
+}
---
+++ DesignPatterns/OO-Imutable/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OO_Imutable
+{
+    class Program
+    {
+        private static bool IsHappyHour;
+
+        static void Main(string[] args)
+        {
+
+
+            Buy(new MoneyAmount(12, "EUR"), 
+                new MoneyAmount(10, "EUR"));
+
+            Buy(new MoneyAmount(7, "EUR"),
+                new MoneyAmount(10, "EUR"));
+
+
+            IsHappyHour = true;
+
+            Buy(new MoneyAmount(7, "EUR"),
+                new MoneyAmount(10, "EUR"));
+
+            var x = new MoneyAmount(2, "EUR");
+            var y = new MoneyAmount(4, "EUR");
+
+            if (x .Equals(y))
+            {
+                Console.WriteLine("Are equal");
+            }
+
+            if (x.Scale(2).Equals(y))
+            {
+                Console.WriteLine("Equal after scaling");
+            }
+
+            HashSet<MoneyAmount> set = new HashSet<MoneyAmount> {x.Scale(2), y};
+            Console.WriteLine(set.Count);
+
+            Console.ReadLine();
+        }
+        
+        static MoneyAmount Reserve(MoneyAmount cost)
+        {
+            decimal factor = 1.0M;
+            if (IsHappyHour)
+            {
+                factor = 0.5M;
+            }
+
+            Console.WriteLine($"Reserving and item that costs {cost}");
+            return cost.Scale(factor);
+        }
+
+        private static void Buy(MoneyAmount wallet, MoneyAmount cost)
+        {
+            var enoughMoney = wallet.Amount > cost.Amount;
+
+            var finalCost = Reserve(cost);
+
+            var finalEnough = wallet.Amount > finalCost.Amount;
+
+            if (enoughMoney)
+            {
+                Console.WriteLine($"You will pay {cost} from your total of {wallet}");
+            }
+            else if (finalEnough)
+            {
+                Console.WriteLine($"This time, you will pay {finalCost} from your total of {wallet}");
+            }
+            else
+            {
+                Console.WriteLine($"You cannot pay {cost} from your total of {wallet}");
+            }
+        }
+    }
+}
