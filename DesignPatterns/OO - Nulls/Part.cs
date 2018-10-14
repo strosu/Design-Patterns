07e1ed0957329bffb1bf9976741b288a2d9c84e9
strosu <rosu@outlook.com>
2018-10-14 15:00:05 +0300

    OO - Optional objects

---
+++ DesignPatterns/OO - Nulls/Part.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OO___Nulls
+{
+    public class Part
+    {
+        public DateTime InstallDate { get; }
+        public DateTime BreakDate { get; private set; }
+
+        public Part(DateTime installDate)
+        {
+            InstallDate = installDate;
+        }
+
+        public void MarkDefective(DateTime breakDate)
+        {
+            BreakDate = breakDate;
+        }
+    }
+}
--- DesignPatterns/OO - Nulls/SoldArticle.cs
+++ DesignPatterns/OO - Nulls/SoldArticle.cs
@@ -8,13 +8,18 @@ namespace OO___Nulls
 
         public Warranty ExpressTimeLimitedWarranty { get; private set; }
 
-        private Warranty NotOperationalWarranty { get; }
+        private readonly Warranty _notOperationalWarranty;
+
+        private Part _circuit;
+        private Warranty _circuitExtendedWarranty;
+        private Warranty _failedCircuitWarranty;
 
         public SoldArticle(Warranty moneyBackGuarantee, Warranty expressTimeLimitedWarranty)
         {
             MoneyBackGuarantee = moneyBackGuarantee ?? throw new ArgumentNullException(nameof(moneyBackGuarantee));
             ExpressTimeLimitedWarranty = VoidWarranty.Instance;
-            NotOperationalWarranty = expressTimeLimitedWarranty ?? throw new ArgumentNullException(nameof(expressTimeLimitedWarranty));
+            _notOperationalWarranty = expressTimeLimitedWarranty ?? throw new ArgumentNullException(nameof(expressTimeLimitedWarranty));
+            _circuitExtendedWarranty = VoidWarranty.Instance;
         }
 
         public void VisibleDamage()
@@ -25,7 +30,24 @@ namespace OO___Nulls
         public void NotOperational()
         {
             MoneyBackGuarantee = VoidWarranty.Instance;
-            ExpressTimeLimitedWarranty = NotOperationalWarranty;
+            ExpressTimeLimitedWarranty = _notOperationalWarranty;
+        }
+
+        public void InstallCircuit(Part circuit, Warranty extendedWarranty)
+        {
+            _circuit = circuit;
+            _failedCircuitWarranty = extendedWarranty;
+        }
+
+        public void CircuitNotOperational(DateTime breakDate)
+        {
+            _circuit.MarkDefective(breakDate);
+            _circuitExtendedWarranty = _failedCircuitWarranty;
+        }
+
+        public void ClaimCircuitWarranty(Action onValidClaim)
+        {
+            _circuitExtendedWarranty.Claim(_circuit.BreakDate, onValidClaim);
         }
     }
 }
