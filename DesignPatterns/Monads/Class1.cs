0347f0ebf821dc63bc9bb13e0a18a65ea83fdcb2
Stefan Rosu <rosu@outlook.com>
2019-03-21 15:30:44 +0200

    monads 1

---
+++ DesignPatterns/Monads/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace Monads
+{
+    public class Class1
+    {
+        delegate T OnDemand<T>();
+
+        static Nullable<T> CreateSimpleNullable<T>(T item) where T: struct
+        {
+            return new Nullable<T>(item);
+        }
+
+        static OnDemand<T> CreateOnDemand<T>(T item)
+        {
+            return () => item;
+        }
+
+        static IEnumerable<T> CreateSimpleSequence<T>(T item)
+        {
+            yield return item;
+        }
+
+        static Task<T> CreateSimpleTask<T>(T item)
+        {
+            return Task.FromResult(item);
+        }
+
+        static Lazy<T> CreateSimpleLazy<T>(T item)
+        {
+            return new Lazy<T>(() => item);
+        }
+    }
+}
