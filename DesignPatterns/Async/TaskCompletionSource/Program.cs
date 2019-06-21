51fbf5ffd28852e3128bea9d435113f3a73cb7c7
strosu <rosu@outlook.com>
2019-06-21 13:33:31 +0300

    TaskCompletionSource 1

---
+++ DesignPatterns/Async/TaskCompletionSource/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaskCompletionSource
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var tasks = new[] {
+                Task.Delay(3000).ContinueWith(_ => 3),
+                Task.Delay(1000).ContinueWith(_ => 1),
+                Task.Delay(2000).ContinueWith(_ => 2),
+                Task.Delay(5000).ContinueWith(_ => 5),
+                Task.Delay(4000).ContinueWith(_ => 4),
+            };
+            //foreach (var bucket in tasks)
+            //{
+            //    var t = await bucket;
+            //    //int result = await t;
+            //    Console.WriteLine("{0}: {1}", DateTime.Now, t);
+            //}
+
+            //foreach (var task in tasks)
+            //{
+            //    var result = await task;
+            //    Console.WriteLine(result);
+            //}
+
+            Console.WriteLine("Ordered:");
+
+            foreach (var task in tasks.OrderByCompletion())
+            {
+                var result = await task;
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static async Task<int> DelayedInt(int delay)
+        {
+            await Task.Delay(delay);
+            return delay;
+        }
+    }
+}
---
+++ DesignPatterns/Async/TaskCompletionSource/TaskExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCompletionSource
+{
+    public static class TaskExtensions
+    {
+        public static IEnumerable<Task<T>> OrderByCompletion<T>(this IEnumerable<Task<T>> initialTasks)
+        {
+            var inputTasks = initialTasks.ToList();
+            var completions = new TaskCompletionSource<T>[inputTasks.Count];
+            int currentIndex = -1;
+
+            for (int i = 0; i < inputTasks.Count; i++)
+            {
+                completions[i] = new TaskCompletionSource<T>();
+                inputTasks[i].ContinueWith(completed =>
+                {
+                    currentIndex = Interlocked.Increment(ref currentIndex);
+                    switch (completed.Status)
+                    {
+                        case TaskStatus.RanToCompletion:
+                            completions[currentIndex].SetResult(completed.Result);
+                            break;
+                        case TaskStatus.Faulted:
+                            completions[currentIndex].SetException(completed.Exception);
+                            break;
+                        case TaskStatus.Canceled:
+                            completions[currentIndex].SetCanceled();
+                            break;
+                    }
+                }, TaskContinuationOptions.ExecuteSynchronously);
+            }
+
+            return completions.Select(x => x.Task);
+        }
+    }
+}
