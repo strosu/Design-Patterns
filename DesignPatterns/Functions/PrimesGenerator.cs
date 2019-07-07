8e5c12a1e6160c0272717cb590dc71c506589c1f
strosu <rosu@outlook.com>
2019-07-07 14:17:52 +0300

    Functions

---
+++ DesignPatterns/Functions/PrimesGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functions
+{
+    public class PrimesGenerator
+    {
+        private static bool[] _crossedOut;
+
+        public static List<int> GeneratePrimes(int maxValue)
+        {
+            if (maxValue < 2)
+            {
+                return new List<int>();
+            }
+
+            UnCrossUpTo(maxValue);
+            CrossOutMultiples();
+            return CopyPrimesToResult();
+        }
+
+        private static void UnCrossUpTo(int maxValue)
+        {
+            _crossedOut = new bool[maxValue + 1];
+        }
+
+        private static void CrossOutMultiples()
+        {
+            _crossedOut[0] = _crossedOut[1] = true;
+            
+            for (int i = 2; i < GetCrossOutLimit(); i++)
+            {
+                if (NotCrossed(i))
+                {
+                    CrossOutMultiplesOf(i);
+                }
+            }
+        }
+
+        private static void CrossOutMultiplesOf(int multiplier)
+        {
+            for (int j = 2 * multiplier; j < _crossedOut.Length; j += multiplier)
+            {
+                _crossedOut[j] = true;
+            }
+        }
+
+        private static bool NotCrossed(int i)
+        {
+            return !_crossedOut[i];
+        }
+
+        private static int GetCrossOutLimit()
+        {
+            return (int) Math.Sqrt(_crossedOut.Length);
+        }
+
+        private static List<int> CopyPrimesToResult()
+        {
+            var result = new List<int>();
+            for (int i = 0; i < _crossedOut.Length; i++)
+            {
+                if (!_crossedOut[i])
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DesignPatterns/Functions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primes = GeneratePrimes(20);
+            foreach (var prime in primes)
+            {
+                Console.WriteLine(prime);
+            }
+
+            Console.WriteLine("--------------------------");
+
+            var primes2 = PrimesGenerator.GeneratePrimes(20);
+            foreach (var prime in primes2)
+            {
+                Console.WriteLine(prime);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] GeneratePrimes(int maxValue)
+        {
+            if (maxValue >= 2)
+            {
+                int s = maxValue + 1;
+                bool[] f = new bool[s];
+                for (int i = 0; i < s; i++)
+                {
+                    f[i] = true;
+                }
+
+                f[0] = f[1] = false;
+
+                for (int i = 2; i < Math.Sqrt(s) + 1; i++)
+                {
+                    if (f[i])
+                    {
+                        for (int j = 2 * i; j < s; j+=i)
+                        {
+                            f[j] = false;
+                        }
+                    }
+                }
+
+                int count = 0;
+                for (int i = 0; i < s; i++)
+                {
+                    if (f[i]) count++;
+                }
+
+                int[] primes = new int[count];
+
+                int k = 0;
+                for (int i = 0; i < s; i++)
+                {
+                    if (f[i])
+                    {
+                        primes[k++] = i;
+                    }
+                }
+
+                return primes;
+            }
+            else
+            {
+                return new int[0];
+            }
+        }
+    }
+}
